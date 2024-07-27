85c6e32678db4d6539fcd54a5af0dad75a7e0215
theperfectpr <kbarua.dev@gmail.com>
2024-07-28 00:09:40 +0530

    add QueryProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { QueryProvider } from "@/providers/query-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,7 +27,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
---
+++ src/providers/query-provider.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+let browserQueryClient: QueryClient | undefined = undefined;
+function makeQueryClient() {
+  return new QueryClient();
+}
+//singleton pattern
+function getQueryClient() {
+  //we are on server
+  if (typeof window === undefined) {
+    return makeQueryClient();
+  } else {
+    //we are on client
+    if (!browserQueryClient) {
+      browserQueryClient = makeQueryClient();
+    }
+    return browserQueryClient;
+  }
+}
+
+const queryClient = getQueryClient();
+
+export function QueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
